8692e63f054aae8e8e638bc3c72faddacfb9630e
myinglin2333 <meiye.l@northeastern.edu>
2026-03-19 17:16:44 -0400

    fix vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import fs from "fs";
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:5001",
-        changeOrigin: true,
+  plugins: [
+    react(),
+
+    {
+      name: "copy-redirects",
+      closeBundle() {
+        try {
+          fs.copyFileSync("public/_redirects", "dist/_redirects");
+          console.log(" _redirects copied to dist");
+        } catch (err) {
+          console.warn(" _redirects not found");
+        }
       },
     },
+  ],
+
+  build: {
+    outDir: "dist",
   },
-})
+});
