320b7a4477a2e47bc72470037613791cd113fac5
myinglin2333 <meiye.l@northeastern.edu>
2026-03-19 17:05:04 -0400

    fix

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,20 @@ const PORT = process.env.PORT || 5001;
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
